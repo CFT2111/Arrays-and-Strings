8005a2a561291688dad7c9a8617b8683127649ab
mmantle-hud <m.e.mantle@hud.ac.uk>
2020-02-06 17:13:01 +0000

    Update arrays.js

--- js/arrays.js
+++ js/arrays.js
@@ -1,28 +1,28 @@
 /*
 1. The following code declares a new array. Using this array attempt the following:
-a) Output the text ‘Tuesday’
-b) Output the text ‘Monday is the first day of the week’
-c) Output the text ‘Friday and Saturday are my favourite days’
-d) Output the entire contents of the array using a for loop
-e) Output the entire contents of the array using a forEach loop
+a) Output the text ‘Tuesday’ (slides 4-7)
+b) Output the text ‘Monday is the first day of the week’ (slides 4-7)
+c) Output the text ‘Friday and Saturday are my favourite days’ (slides 4-7)
+d) Output the entire contents of the array using a for loop (slide 11)
+e) Output the entire contents of the array using a forEach loop (slide 12)
 */
 
 const daysOfTheWeek= ["Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday","Sunday"];
 
 /*
 2. The following code declares an array. Using this array attempt the following:
-a) Output the entire contents of the array using a forEach loop
-b) Use a filter to generate an array of countries that contain the letter 'n'. Have a look at using indexOf() (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/indexOf).  Save the resulting array and output its contents using a forEach loop
+a) Output the entire contents of the array using a forEach loop (slide 12)
+b) Use a filter to generate an array of countries that contain the letter 'n'. Have a look at using indexOf() (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/indexOf).  Save the resulting array and output its contents using a forEach loop (slide 15, slide 18) 
 c) Modify your program from (2b) so that you ask the user for a search term and then display the countries that match the search term e.g. if the user enters 'in', China, India and Indonesia should be displayed.
 */
 const countries=["China","India","USA","Indonesia","Brazil","Pakistan"];
 
 /*
 3. The following code declares an array of numbers. Using this array attempt the following:
-a) Output the entire contents of the array using a forEach loop
-b) Use a map to double each element in the array. Save the resulting array as doubleNums and output its contents using a forEach loop
-c) Use a filter to generate an array of positive numbers based on nums. Save the resulting array as positiveNums and output its contents using a forEach loop
+a) Output the entire contents of the array using a forEach loop (slide 12)
+b) Use a map to double each element in the array. Save the resulting array as doubleNums and output its contents using a forEach loop (slide 14)
+c) Use a filter to generate an array of positive numbers based on nums. Save the resulting array as positiveNums and output its contents using a forEach loop (slide 15)
 d) Instead of using a 'hard-coded' array of numbers, use a for loop to repeatedly ask the user for a number. Each time the user answers with a number, store the answer in the nums array. Using this array output the double of each element, and all the positive numbers that were entered.
-e) Use the reduce function to get the total of all the numbers entered.
-f) Use reduce to get the largest of the numbers that have been entered.
+e) Use the reduce function to get the total of all the numbers entered. (slide 16)
+f) Use reduce to get the largest of the numbers that have been entered. (slide 16)
 */
